c2d52b6ca0644290d32adda47e0c7a229dbd2d55
Sory Escobar <sory@MacBook-Pro-de-Sory.local>
2020-11-15 20:31:40 -0500

    0.0.1 - Fixed the enabler.sh script

--- enabler.sh
+++ enabler.sh
@@ -3,8 +3,10 @@
 ## Sory's Dot Files Enabler ##
 ##############################
 
-ln -s  .bash_profile        ~/.bash_profile 
-ln -s  .alacritty.yml       ~/.alacritty.yml
-ln -s  .tmux.conf           ~/.tmux.conf
-ln -s  .vimrc               ~/.vimrc
-ln -s  .vim                 ~/.vim
+
+
+ln -sf  `pwd`/.bash_profile        ~/.bash_profile 
+ln -sf  `pwd`/.alacritty.yml       ~/.alacritty.yml
+ln -sf  `pwd`/.tmux.conf           ~/.tmux.conf
+ln -sf  `pwd`/.vimrc               ~/.vimrc
+ln -sf  `pwd`/.vim                 ~/.vim
